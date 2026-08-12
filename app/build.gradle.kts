f9be770e96a1f372625974832c15865ecfb5838f
Tanmay S. <nitrotanmay538@gmail.com>
2026-08-12 18:35:38 +0530

    Update versionName to 1.6.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 5
-        versionName = "1.5.1"
+        versionName = "1.6.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true

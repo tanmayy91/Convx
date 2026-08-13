5e66e865fd85738b8c25336bb70c92024d1c4e14
Wohecem715 <61895353-wohecem715@users.noreply.replit.com>
2026-08-13 02:24:44 +0000

    Bump app version to 1.7.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 5
-        versionName = "1.6.2"
+        versionName = "1.7.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
